f0086eae2ff4dd3e1f5e3ca302d5cbab60869bb8
wuyuanwuhui99 <275018723@qq.com>
2025-03-09 17:54:33 +0800

    修改音乐搜索页面

--- src/pages/MusicSearchPage.vue
+++ src/pages/MusicSearchPage.vue
@@ -1,41 +1,46 @@
 <template>
 	<view class="page-wrapper" scroll-y show-scrollbar="false">
-		<view class="module-block module-block-row">
-			<view class="search-input-wrapper">
-				<input class="search-input" v-model="keyword" :placeholder="placeholder" />
-				<image @click="useClear" v-if="keyword" class="icon-clear" :src="icon_clear" />
+		<NavigatorTitleComponent title="我喜欢的歌曲"/>
+		<view class="page-body">
+			<view class="module-block module-block-row">
+				<view class="search-input-wrapper">
+					<input class="search-input" v-model="keyword" :placeholder="placeholder" />
+					<image @click="useClear" v-if="keyword" class="icon-clear" :src="icon_clear" />
+				</view>
+				<text class="search-btn" @click="useSearch">搜索</text>
 			</view>
-			<text class="search-btn" @click="useSearch">搜索</text>
-		</view>
 
-		<template v-if="searching">
-			<scroll-view v-if="!loading" class="module-block module-block-last scroll-list" scroll-y
-				show-scrollbar="false">
-				<view v-for="item in searchMusicList" :key="item.id" class="scroll-view-item">
-					<image class="song-cover" :src="getMusicCover(item.cover)" />
-					<MusicAvaterComponent type="music" class="song-cover" :name="item.songName" :avater="item.cover"/>
-					<view class="name-wrapper">
-						<text class="song-name">{{ item.songName }}</text>
-						<text class="author-name">{{ item.authorName }}</text>
+			<template v-if="searching">
+				<scroll-view v-if="!loading" scroll-y class="scroll-list" show-scrollbar="false" @scrolltolower="onScrolltolower">
+					<view class="module-block module-block-last" >
+						<view v-for="item in searchMusicList" :key="item.id" class="scroll-view-item">
+							<MusicAvaterComponent type="music" class="song-cover" :name="item.songName" :avater="item.cover"/>
+							<view class="name-wrapper">
+								<text class="song-name">{{ item.songName }}</text>
+								<text class="author-name">{{ item.authorName }}</text>
+							</view>
+							<image @click="usePlayMusic(item)" class="icon-play" :src=" store.isPlaying && store.musicItem.id === item.id ? playingIcon : pauseIcon" />
+							<image class="icon-play" :src="item.isLike ? isLikeActiveIcon : isLikeIcon" />
+							<image class="icon-play" :src="icon_music_menu" />
+						</view>
 					</view>
-					<image @click="usePlayMusic(item)" class="icon-play" :src=" store.isPlaying && store.musicItem.id === item.id ? playingIcon : pauseIcon" />
-					<image class="icon-play" :src="item.isLike ? isLikeActiveIcon : isLikeIcon" />
-					<image class="icon-play" :src="icon_music_menu" />
+					<text class="footer">{{ total > pageNum * PAGE_SIZE ? '正在加载更多' : '已经到底了'}}</text>
+				</scroll-view>
+			</template>
+			<template v-else>
+				<view class="module-block" style="padding-right: 0;">
+					<TitleComponent title="历史记录" />
+					<view class="record-list">
+						<template v-if="searchRecordList.length > 0">
+							<text @click="useLabelSearch(item)" class="record-item" v-for="item,index in searchRecordList"
+								:key="item+index">{{item}}</text>
+						</template>
+						<text class="no-data">暂无搜索记录</text>
+					</view>
 				</view>
-			</scroll-view>
-		</template>
-		<template v-else>
-			<view class="module-block" style="padding-right: 0;">
-				<TitleComponent title="历史记录" />
-				<view class="record-list">
-					<template v-if="searchRecordList.length > 0">
-						<text @click="useLabelSearch(item)" class="record-item" v-for="item,index in searchRecordList"
-							:key="item+index">{{item}}</text>
-					</template>
-					<text class="no-data">暂无搜索记录</text>
-				</view>
-			</view>
-		</template>
+			</template>
+		</view>
+		
 	</view>
 </template>
 
@@ -43,7 +48,8 @@
 	import { reactive, ref } from 'vue';
 	import { useRoute } from "vue-router";
 	import type { MusicType } from '../types';
-	import { MUSIC_SEARCH_STORAGE_KEY } from '../common/constant';
+	import { MUSIC_SEARCH_STORAGE_KEY, PAGE_SIZE } from '../common/constant';
+	import NavigatorTitleComponent from '../components/NavigatorTitleComponent.vue';
 	import TitleComponent from '../components/TitleComponent.vue';
 	import { searchMusicService } from '../service';
 	import { useStore } from "../stores/useStore";
@@ -51,11 +57,9 @@
 	import playingIcon from '../../static/icon_music_playing_grey.png';
 	import isLikeIcon from '../../static/icon_like.png';
 	import isLikeActiveIcon from '../../static/icon_like_active.png';
-	import { getMusicCover } from '../utils/util';
 	import MusicAvaterComponent from '../components/MusicAvaterComponent.vue';
 	import icon_music_menu from '../../static/icon_music_menu.png';
 	import icon_clear from '../../static/icon_clear.png';
-	
 	
 	const store = useStore();
 	const route = useRoute();
@@ -69,6 +73,7 @@
 	const keyword = ref<string>("");
 	const pageNum = ref<number>(1);
 	const pageSize = ref<number>(20);
+	const total = ref<number>(0);
 
 	uni.getStorage({ key: MUSIC_SEARCH_STORAGE_KEY }).then(res => {
 		if (res) {
@@ -107,6 +112,7 @@
 		uni.setStorage({ key: MUSIC_SEARCH_STORAGE_KEY, data: searchRecordList.join(",") })
 		searchMusicService(keyword.value, pageNum.value, pageSize.value).then(res => {
 			searchMusicList.splice(0, searchMusicList.length, ...res.data);
+			total.value = res.total;
 		}).finally(() => {
 			loading.value = false
 		})
@@ -132,6 +138,20 @@
 		keyword.value = item;
 		useSearch();
 	}
+
+	const onScrolltolower = ()=>{
+		if (loading.value) return;
+		if (total.value > PAGE_SIZE * pageNum.value) {
+			pageNum.value++;
+			uni.showLoading();
+			searchMusicService(keyword.value, pageNum.value, pageSize.value).then(res => {
+				searchMusicList.push(...res.data);
+			}).finally(() => {
+				uni.hideLoading();
+				loading.value = false;
+			})
+		}
+	}
 </script>
 
 <style lang="less" scoped>
@@ -144,124 +164,133 @@
 		display: flex;
 		overflow: hidden;
 		flex-direction: column;
-		padding: 0 @page-padding @page-padding;
 		background-color: @page-background-color;
 		box-sizing: border-box;
-
-		/deep/.uni-scroll-view::-webkit-scrollbar {
-			display: none;
-		}
-
-		.module-block-row {
-			.search-input-wrapper {
-				flex: 1;
-				background-color: @page-background-color;
-				height: @input-height;
-				border-radius: @input-height;
-				padding-left: @page-padding;
-				padding-right: @page-padding;
-				display: flex;
-				flex-direction: row;
-				align-items: center;
-
-				.search-input {
-					flex: 1;
-				}
-
-				.icon-clear {
-					width: @small-icon-size;
-					height: @small-icon-size;
-				}
-			}
-
-			.search-btn {
-				display: inline-block;
-				background-color: @line-color;
-				height: @input-height;
-				color: @module-background-color;
-				display: flex;
-				justify-content: center;
-				align-items: center;
-				width: 20%;
-				margin-left: @page-padding;
-				border-radius: @input-height;
-			}
-		}
-
-		.record-list {
-			display: flex;
-			flex-wrap: wrap;
-
-			.record-item {
-				margin-top: @page-padding;
-				margin-right: @page-padding;
-				height: @input-height;
-				background-color: @page-background-color;
-				display: flex;
-				justify-content: center;
-				align-items: center;
-				padding: 0 @btn-padding;
-				border-radius: @input-height;
-			}
-
-			.no-data {
-				display: flex;
-				width: 100%;
-				height: @big-avater;
-				justify-content: center;
-				align-items: center;
-			}
-		}
-
-		.scroll-list {
-			flex: 1;
-			padding: @page-padding;
-			box-sizing: border-box;
+		.page-body{
 			height: 0;
-
-			.scroll-view-item {
-				display: flex;
-				margin-top: @page-padding;
-				padding-bottom: @page-padding;
-				border-bottom: 1rpx solid @search-input-color;
-				align-items: center;
-
-				&:first-child {
-					margin-top: 0;
-				}
-
-				&:last-child {
-					border-bottom: none;
-				}
-
-				.song-cover {
-					margin-right: @page-padding;
-				}
-
-				.name-wrapper {
-					display: flex;
-					flex-direction: column;
-					gap: @small-margin;
+			flex: 1;
+			padding: 0 @page-padding;
+			display: flex;
+			flex-direction: column;
+			.module-block-row {
+				.search-input-wrapper {
 					flex: 1;
-					width: 0;
+					background-color: @page-background-color;
+					height: @input-height;
+					border-radius: @input-height;
+					padding-left: @page-padding;
+					padding-right: @page-padding;
+					display: flex;
+					flex-direction: row;
+					align-items: center;
 
-					.song-name {
-						text-overflow: ellipsis;
-						white-space: nowrap;
-						overflow: hidden;
+					.search-input {
+						flex: 1;
 					}
 
-					.author-name {
-						color: @sub-title-color;
+					.icon-clear {
+						width: @small-icon-size;
+						height: @small-icon-size;
 					}
 				}
 
-				.icon-play {
+				.search-btn {
+					display: inline-block;
+					background-color: @line-color;
+					height: @input-height;
+					color: @module-background-color;
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					width: 20%;
 					margin-left: @page-padding;
-					width: @small-icon-size;
-					height: @small-icon-size;
+					border-radius: @input-height;
+				}
+			}
+			.module-block-last{
+				margin-top: 0;
+			}
+			.record-list {
+				display: flex;
+				flex-wrap: wrap;
+
+				.record-item {
+					margin-top: @page-padding;
+					margin-right: @page-padding;
+					height: @input-height;
+					background-color: @page-background-color;
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					padding: 0 @btn-padding;
+					border-radius: @input-height;
+				}
+
+				.no-data {
+					display: flex;
+					width: 100%;
+					height: @big-avater;
+					justify-content: center;
+					align-items: center;
+				}
+			}
+
+			.scroll-list {
+				flex: 1;
+				box-sizing: border-box;
+				height: 0;
+				margin-top: @page-padding;
+				.footer {
+					width: 100%;
+					padding: @page-padding;
+					text-align: center;
+					display: inline-block;
+				}
+				.scroll-view-item {
+					display: flex;
+					margin-top: @page-padding;
+					padding-bottom: @page-padding;
+					border-bottom: 1rpx solid @search-input-color;
+					align-items: center;
+
+					&:first-child {
+						margin-top: 0;
+					}
+
+					&:last-child {
+						border-bottom: none;
+					}
+
+					.song-cover {
+						margin-right: @page-padding;
+					}
+
+					.name-wrapper {
+						display: flex;
+						flex-direction: column;
+						gap: @small-margin;
+						flex: 1;
+						width: 0;
+
+						.song-name {
+							text-overflow: ellipsis;
+							white-space: nowrap;
+							overflow: hidden;
+						}
+
+						.author-name {
+							color: @sub-title-color;
+						}
+					}
+
+					.icon-play {
+						margin-left: @page-padding;
+						width: @small-icon-size;
+						height: @small-icon-size;
+					}
 				}
 			}
 		}
+		
 	}
 </style>
--- src/pages/UserPage.vue
+++ src/pages/UserPage.vue
@@ -1,34 +1,38 @@
 <template>
 	<view class="page-wrapper">
-		<view class="module-block">
-			<view class="row">
-				<text class="text">头像</text>
-				<image class="big-avater" :src="store.userData.avater ? HOST + store.userData.avater:defaulAvater"/>
-				<image class="icon-arrow" :src="icon_arrow"/>
+		<NavigatorTitleComponent title="个人信息"/>
+		<view class="page-body">
+			<view class="module-block">
+				<view class="row">
+					<text class="text">头像</text>
+					<image class="big-avater" :src="store.userData.avater ? HOST + store.userData.avater:defaulAvater"/>
+					<image class="icon-arrow" :src="icon_arrow"/>
+				</view>
+				<view class="row" @click="useEditUserData('昵称','username')">
+					<text class="text">昵称</text>
+					<text>{{store.userData.username}}</text>
+					<image class="icon-arrow" :src="icon_arrow"/>
+				</view>
+				<view class="row" @click="useEditUserData('邮箱','email')">
+					<text class="text">邮箱</text>
+					<text>{{store.userData.email}}</text>
+					<image class="icon-arrow" :src="icon_arrow"/>
+				</view>
+				<view class="row" @click="useEditSex">
+					<text class="text">性别</text>
+					<text>{{SexMap[store.userData.sex] || ''}}</text>
+					<image class="icon-arrow" :src="icon_arrow"/>
+				</view>
+				<view class="row last-row" @click="useEditUserData('个性签名','sign')">
+					<text class="text">个性签名</text>
+					<text>{{store.userData.sign}}</text>
+					<image class="icon-arrow" :src="icon_arrow"/>
+				</view>
 			</view>
-			<view class="row" @click="useEditUserData('昵称','username')">
-				<text class="text">昵称</text>
-				<text>{{store.userData.username}}</text>
-				<image class="icon-arrow" :src="icon_arrow"/>
-			</view>
-			<view class="row" @click="useEditUserData('邮箱','email')">
-				<text class="text">邮箱</text>
-				<text>{{store.userData.email}}</text>
-				<image class="icon-arrow" :src="icon_arrow"/>
-			</view>
-			<view class="row" @click="useEditSex">
-				<text class="text">性别</text>
-				<text>{{SexMap[store.userData.sex] || ''}}</text>
-				<image class="icon-arrow" :src="icon_arrow"/>
-			</view>
-			<view class="row last-row" @click="useEditUserData('个性签名','sign')">
-				<text class="text">个性签名</text>
-				<text>{{store.userData.sign}}</text>
-				<image class="icon-arrow" :src="icon_arrow"/>
-			</view>
+			<button class="btn-logout" @click="useLogout">退出登录</button>
+			<view class="btn-update-password" @click="useUpdatePassword">修改密码</view>
 		</view>
-		<button class="btn-logout" @click="useLogout">退出登录</button>
-		<view class="btn-update-password" @click="useUpdatePassword">修改密码</view>
+		
 		<uni-popup ref="popup1" class="popup-wrapper"  type="dialog">
 			<view class="dialog-wrapper">
 				<view class="dialog-box">
@@ -59,6 +63,7 @@
 	import defaulAvater from '../../static/default_avater.png';
 	import { SexMap } from '../common/config';
 	import icon_arrow from "../../static/icon_arrow.png"
+	import NavigatorTitleComponent from '../components/NavigatorTitleComponent.vue';
 
 	const title = ref<string>('');
 	const field = ref<string>('');
@@ -164,145 +169,150 @@
 		height: 100%;
 		display: flex;
 		flex-direction: column;
-		padding: 0 @page-padding @page-padding;
 		background-color: @page-background-color;
 		box-sizing: border-box;
-		.row{
-			display: flex;
-			padding: @page-padding;
-			align-items: center;
-			border-bottom: 1rpx solid @page-background-color;
-			&.last-row{
-				border-bottom:none
-			}
-			.text{
-				flex: 1;
-			}
-			.big-avater{
-				width: @big-avater;
-				height: @big-avater;
-				border-radius: @big-avater;
-			}
-			.icon-arrow{
-				margin-left: @page-padding;
-				width: @small-icon-size;
-				height: @small-icon-size;
-				opacity: 0.5;
-			}
-		}
-		.btn-logout{
-			border: none;
-			display: block;
-			width: 100%;
-			margin-top: @page-padding;
-			background-color: @warn-color;
-			color: @module-background-color;
-			border-radius: @big-border-radius;
-		}
-		.btn-update-password{
-			text-align: center;
-			width: 100%;
-			outline: none;
-			border: 1rpx solid @disable-text-color;
-			box-sizing: border-box;
-			border-radius: @big-border-radius;
-			margin-top:  @page-padding;
-			display: inline-block;
-			padding: @page-padding ;
-			background-color: transparent;
-		}
-		.popup-wrapper{
-			width: 100%;
-			height: 100%;
-			background-color: rgba(0,0,0,0.2);
-			top: 0;
-			left: 0;
-			.dialog-wrapper{
-				display: flex;
-				height: 100vh;
-				width: 100%;
-				align-items: center;
-				justify-content: center;
-				&.sex-dialog-wrapper{
-					flex-direction: column;
-					justify-content: flex-end;
+		.page-body{
+			margin: 0 @page-padding;
+			.module-block{
+				.row{
+					display: flex;
 					padding: @page-padding;
-					box-sizing: border-box;
-					.option-wrapper{
-						display: flex;
+					align-items: center;
+					border-bottom: 1rpx solid @page-background-color;
+					&.last-row{
+						border-bottom:none
+					}
+					.text{
+						flex: 1;
+					}
+					.big-avater{
+						width: @big-avater;
+						height: @big-avater;
+						border-radius: @big-avater;
+					}
+					.icon-arrow{
+						margin-left: @page-padding;
+						width: @small-icon-size;
+						height: @small-icon-size;
+						opacity: 0.5;
+					}
+				}
+			}
+			.btn-logout{
+				border: none;
+				display: block;
+				width: 100%;
+				margin-top: @page-padding;
+				background-color: @warn-color;
+				color: @module-background-color;
+				border-radius: @big-border-radius;
+			}
+			.btn-update-password{
+				text-align: center;
+				width: 100%;
+				outline: none;
+				border: 1rpx solid @disable-text-color;
+				box-sizing: border-box;
+				border-radius: @big-border-radius;
+				margin-top:  @page-padding;
+				display: inline-block;
+				padding: @page-padding ;
+				background-color: transparent;
+			}
+			.popup-wrapper{
+				width: 100%;
+				height: 100%;
+				background-color: rgba(0,0,0,0.2);
+				top: 0;
+				left: 0;
+				.dialog-wrapper{
+					display: flex;
+					height: 100vh;
+					width: 100%;
+					align-items: center;
+					justify-content: center;
+					&.sex-dialog-wrapper{
 						flex-direction: column;
-						background-color: @module-background-color;
-						width: 100%;
-						border-radius: @module-border-radius;
-						.option-item{
-							display: block;
+						justify-content: flex-end;
+						padding: @page-padding;
+						box-sizing: border-box;
+						.option-wrapper{
+							display: flex;
+							flex-direction: column;
+							background-color: @module-background-color;
+							width: 100%;
+							border-radius: @module-border-radius;
+							.option-item{
+								display: block;
+								width: 100%;
+								text-align: center;
+								padding: @page-padding 0;
+								border-bottom: 1rpx solid @page-background-color;
+								&:last-child{
+									border-bottom:none;
+								}
+							}
+						}
+						.option-btn{
+							margin-top: @page-padding;
 							width: 100%;
 							text-align: center;
 							padding: @page-padding 0;
-							border-bottom: 1rpx solid @page-background-color;
-							&:last-child{
-								border-bottom:none;
-							}
-						}
-					}
-					.option-btn{
-						margin-top: @page-padding;
-						width: 100%;
-						text-align: center;
-						padding: @page-padding 0;
-						background-color: @module-background-color;
-						border-radius: @module-border-radius;
-					}
-				}
-				.dialog-box{
-					width: 80%;
-					left: 50%;
-					top: 50%;
-					border-radius: @module-border-radius;
-					background-color: @white-background-color;
-					.dialog-header{
-						padding: @page-padding;
-						display: flex;
-						align-items: center;
-						justify-content: center;
-						font-weight: bold;
-						border-bottom: 1rpx solid @page-background-color;
-					}
-					.dialog-content{
-						padding: @page-padding;
-						display: flex;
-						align-items: center;
-						.field-text{
-							margin-right: @page-padding;
-						}
-						.text-input{
-							flex: 1;
-							background-color: @page-background-color;
-							padding: @page-padding;
-							color: @sub-title-color;
+							background-color: @module-background-color;
 							border-radius: @module-border-radius;
 						}
 					}
-					.dialog-btn{
-						display: flex;
-						border-top: 1rpx solid @page-background-color;
-						.btn-cancle{
-							flex: 1;
+					.dialog-box{
+						width: 80%;
+						left: 50%;
+						top: 50%;
+						border-radius: @module-border-radius;
+						background-color: @white-background-color;
+						.dialog-header{
 							padding: @page-padding;
-							text-align: center;
-							color: @sub-title-color;
-						}
-						.btn-sure{
-							flex: 1;
-							padding: @page-padding;
-							text-align: center;
-							color: @line-color;
+							display: flex;
+							align-items: center;
+							justify-content: center;
 							font-weight: bold;
-							border-left: 1rpx solid @page-background-color;;
+							border-bottom: 1rpx solid @page-background-color;
+						}
+						.dialog-content{
+							padding: @page-padding;
+							display: flex;
+							align-items: center;
+							.field-text{
+								margin-right: @page-padding;
+							}
+							.text-input{
+								flex: 1;
+								background-color: @page-background-color;
+								padding: @page-padding;
+								color: @sub-title-color;
+								border-radius: @module-border-radius;
+							}
+						}
+						.dialog-btn{
+							display: flex;
+							border-top: 1rpx solid @page-background-color;
+							.btn-cancle{
+								flex: 1;
+								padding: @page-padding;
+								text-align: center;
+								color: @sub-title-color;
+							}
+							.btn-sure{
+								flex: 1;
+								padding: @page-padding;
+								text-align: center;
+								color: @line-color;
+								font-weight: bold;
+								border-left: 1rpx solid @page-background-color;;
+							}
 						}
 					}
 				}
 			}
 		}
+		
 	}
 </style>
